5a97b1c4368119240bea00cebf2dc445b1817f25
pengwei <pengwei@antiy.cn>
2018-09-19 15:01:17 +0800

    init

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { exec } = require('child_process');
 
-const cmd = `git add . | git commit -m 'init'| git pull origin master | git push origin master`
+// const cmd = `git add . | git commit -m 'init'| git pull origin master | git push origin master`
+const cmd = `git add. | git commit -m 'init' | git pull origin master`
 
 exec(cmd, (err, stdout, stderr) => {
   if (err) {
