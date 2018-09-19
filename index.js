4e736208ae7b54449110a36a52b477e3315876c1
pengwei <pengwei@antiy.cn>
2018-09-19 15:13:37 +0800

    xx

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const { exec } = require('child_process');
+const util = require("util");
+
+// const { exec } = require('child_process');
+
+const exec = util.promisify(require("child_process").exec);
 
 // const cmd = `git add . | git commit -m 'init'| git pull origin master | git push origin master`
-const cmd = `git add. | git commit -m 'init' | git pull origin master`
+// const cmd = `git add. | git commit -m 'init' | git pull origin master`
 
-exec(cmd, (err, stdout, stderr) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log('stdout: ',stdout, "\n", "stderr: ",stderr);
-});
+// exec(cmd, (err, stdout, stderr) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log('stdout: ',stdout, "\n", "stderr: ",stderr);
+// });
+
+
+async function git(cmd){
+  let obj =  await exec(cmd);
+  // if()
+}
