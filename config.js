688a7157c046e6eb1982f09e16caf33fbed3a324
pengwei <pengwei@antiy.cn>
2018-09-19 16:05:54 +0800

    add test

---
+++ config.js
@@ -0,0 +1,6 @@
+
+
+module.exports = {
+  branch:"master",
+  
+}
---
+++ test.js
@@ -0,0 +1,10 @@
+
+
+// const util = require("util");
+// const exec = util.promisify(require("child_process").exec);
+
+// exec("pwd")
+// .then(res => console.log("res", res) )
+// .catch(err => console.log("err", err ) );
+
+console.log(process)
